c9d1bb731c5970111c06ffd5ee8d7bb42e382f79
Nayra <nayradb@gmail.com>
2020-04-17 10:39:53 +0200

    Version 1.0.0 first commit

---
+++ src/eed/ejercicios/git/Nayra.java
@@ -0,0 +1,13 @@
+/**
+ * 
+ */
+package eed.ejercicios.git;
+
+/**
+ * @author Nayra
+ *
+ */
+public class Nayra {
+	public static String saludo ="Nayra: Los que van a morir te saludan.";
+	public static String despedida = "Nayra: que la fuerza os acompañe.";
+}
---
+++ src/eed/ejercicios/git/saludos.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package eed.ejercicios.git;
+
+/**
+ * @author Nayra
+ *
+ */
+public class saludos {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(Nayra.saludo);
+		System.out.println(Nayra.despedida);	
+
+	}
+
+}
