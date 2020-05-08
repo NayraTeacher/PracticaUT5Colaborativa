9d7d63746d2b008f7d871674103264159b9e2061
Danyj <Danyj@DESKTOP-B7H30RE>
2020-05-08 10:31:28 +0200

    Hola

---
+++ src/eed/ejercicios/git/Danilo.java
@@ -0,0 +1,8 @@
+package eed.ejercicios.git;
+
+public class Danilo {
+
+	public static String saludo = "Danilo: pasaba por aqui para saludaros a todos.";
+	public static String despedida = "Danilo: me tengo que ir, os figo adios.";
+
+}
