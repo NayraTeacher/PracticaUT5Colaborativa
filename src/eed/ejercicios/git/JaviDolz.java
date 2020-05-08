ad343d1eef94043eadd0624f5452242367c6f8b9
jdolzm <j.dolz6549@gmail.com>
2020-05-08 10:51:39 +0200

    version 1.0 rama nueva

---
+++ src/eed/ejercicios/git/JaviDolz.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package eed.ejercicios.git;
+
+/**
+ *
+ * @author jdolzm
+ */
+public class JaviDolz {
+
+    public static String saludo = "JaviDolz: Los que van a morir te saludan.";
+    public static String despedida = "JaviDolz: que la fuerza os acompa�e.";
+}
