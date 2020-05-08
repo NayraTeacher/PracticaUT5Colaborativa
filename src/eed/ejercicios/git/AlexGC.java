ff507d211665df9a37ecd0df9cfe163cdd356199
usuario <usuario@LAPTOP-EBET0HUE>
2020-05-08 10:46:56 +0200

    Crear clase Alexgc

---
+++ src/eed/ejercicios/git/AlexGC.java
@@ -0,0 +1,7 @@
+package eed.ejercicios.git;
+
+public class AlexGC {
+	public static String saludo = "AlexGC: Los que van a morir te saludan.";
+	public static String despedida = "AlexGC: Los que van a morir te saludan.";
+
+}
