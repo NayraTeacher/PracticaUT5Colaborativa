7e68797352d302552a957ea3dc8864b1358efc10
Mikel Conde <Mikel Conde@DESKTOP-TAFP0TJ>
2020-05-08 10:28:35 +0200

    ee

--- src/eed/ejercicios/git/Mikel.java
+++ src/eed/ejercicios/git/Mikel.java
@@ -3,6 +3,6 @@ package eed.ejercicios.git;
 public class Mikel {
 
 	
-		public static String saludo ="Mikel: Los que van a morir te saludan.";
+		public static String saludo ="Mikel:Los que van a morir te saludan e.";
 		public static String despedida = "Mikel: que la fuerza os acompañe.";
 	}
