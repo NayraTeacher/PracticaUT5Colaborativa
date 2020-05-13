4ebf9645cdbbf8500edff004ad4b777cd729a9b9
almulaprofedeinformatica <almulaprofedeinformatica@gmail.com>
2020-05-13 20:26:21 +0200

    Cambios de Almudena

---
+++ src/eed/ejercicios/git/Almu.java
@@ -0,0 +1,6 @@
+package eed.ejercicios.git;
+
+public class Almu {
+	public static String saludo = "Almu: Los que van a morir te saludan.";
+	public static String despedida = "Almu: que la fuerza os acompañe.";
+}
